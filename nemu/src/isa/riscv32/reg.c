40e64b81765a7d7f07c9308bd1dd8e8c766c64da
tracer-ics2019 <tracer@njuics.org>
2025-12-24 19:11:13 +0800

    >  compile U202215670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:11:13 up  3:08,  1 user,  load average: 0.11, 0.08, 0.08 e07469f1a053e051b9a1be6938f7b8cedd239e2

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -8,6 +8,10 @@ const char *regsl[] = {
 };
 
 void isa_reg_display() {
+      for(int i=0;i<32;i++)
+      {
+       printf("%s  ->  0x%08x  %u\n", reg_name(i, 0), reg_l(i), reg_l(i));
+      }
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
