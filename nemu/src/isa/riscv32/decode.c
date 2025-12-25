ad8d827ba1a27f2fcbf776c3efaf27de36dc860d
tracer-ics2019 <tracer@njuics.org>
2025-12-25 17:24:43 +0800

    >  compile U202215670 hust Linux hust-desktop 5.4.0-45-generic #49-Ubuntu SMP Wed Aug 26 13:38:52 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:24:43 up 29 min,  1 user,  load average: 0.34, 0.20, 0.18 6f7b7d9152cfeb556c0f97284d06f5706250d7cd

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -21,6 +21,7 @@ static inline make_DopHelper(r) {
 
   print_Dop(op->str, OP_STR_SIZE, "%s", reg_name(op->reg, 4));
 }
+
 make_DHelper (R) {
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_r(id_src2, decinfo.isa.instr.rs2, true);
@@ -46,21 +47,20 @@ make_DHelper (B) {
   decode_op_i(id_dest, simm, true);
 }
 
+make_DHelper (U) {
+  decode_op_i(id_src, decinfo.isa.instr.imm31_12 << 12, true);
+  decode_op_r(id_dest, decinfo.isa.instr.rd, false);
+  
+  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", decinfo.isa.instr.imm31_12);
+}
+
 make_DHelper (J) {
   int32_t simm = (decinfo.isa.instr.simm20 << 20) | (decinfo.isa.instr.imm19_12 << 12) | (decinfo.isa.instr.imm11_ << 11) | (decinfo.isa.instr.imm10_1 << 1);
   decode_op_i(id_src, simm, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
 
-
-make_DHelper(U) {
-  decode_op_i(id_src, decinfo.isa.instr.imm31_12 << 12, true);
-  decode_op_r(id_dest, decinfo.isa.instr.rd, false);
-
-  print_Dop(id_src->str, OP_STR_SIZE, "0x%x", decinfo.isa.instr.imm31_12);
-}
-
-make_DHelper(ld) {
+make_DHelper (ld) {
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
 
@@ -71,7 +71,7 @@ make_DHelper(ld) {
   decode_op_r(id_dest, decinfo.isa.instr.rd, false);
 }
 
-make_DHelper(st) {
+make_DHelper (st) {
   decode_op_r(id_src, decinfo.isa.instr.rs1, true);
   int32_t simm = (decinfo.isa.instr.simm11_5 << 5) | decinfo.isa.instr.imm4_0;
   decode_op_i(id_src2, simm, true);
